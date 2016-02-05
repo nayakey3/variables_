4987b675ca0a2d0f066ad897e5c021efa29509dc
janeeats <jane.vora@gmail.com>
2016-02-05 02:36:48 +0000

    create starter code

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+// this click function increments the current score by 1
+  $("#incrementBy1").click(function(){
+    var score = $('#current').text();
+    score++;
+    $("#current").text(score);
+
+// write click function to decrement by 1
+
+
+// write click function to increment by 5
+
+
+// write click function to decrement by 5
+
+
+
+  });
+});
