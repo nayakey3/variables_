bbc897c3bb3b1614842ea6f28f2a9015348b2937
nayakey3 <nayakey123@gmail.com>
2016-08-05 14:30:15 +0000

    push to github

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ $(document).ready(function(){
     var score = $('#current').text();
     score++;
     $("#current").text(score);
-
+  });
 // write click function to decrement by 1
-
+$("#decrementBy1").click(function(){
+  var score = $('#current').text();
+  score--;
+  $("#current").text(score);
+});
 
 // write click function to increment by 5
-
+$("#incrementBy5").click(function(){
+  var score = $('#current').text();
+  score = Number(score);
+  score = score + 5;
+  $("#current").text(score);
+});
 
 // write click function to decrement by 5
+$("#decrementBy5").click(function(){
+  var score = $('#current').text();
+  score = Number(score - 5);
+  $("#current").text(score);
+});
 
 
-
-  });
 });
